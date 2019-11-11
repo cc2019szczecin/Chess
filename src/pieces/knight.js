dd73270116311a448e35695ffc5554f934dd0794
Marta Jaszewska <jaszewska.marta@gmail.com>
2019-11-11 13:58:44 +0100

    possible moves

--- src/pieces/knight.js
+++ src/pieces/knight.js
@@ -7,8 +7,18 @@ class Knight extends Piece {
     this.display = `<i class="fas fa-chess-knight ${side}"></i>`; //fontawesome knight
   }
   findLegalMoves() {
-    const possibleMoves = [];
 
+    const possibleMoves = [];
+    
+      (this.x + 2 >= 0 && this.y + 1 >= 0 && this.x + 2 <= 7 && this.y + 1 <= 7 ) && possibleMoves.push(`${this.x + 2},${this.y + 1}`);
+      (this.x + 2 >= 0 && this.y - 1 >= 0 && this.x + 2 <= 7 && this.y - 1 <= 7 ) && possibleMoves.push(`${this.x + 2},${this.y - 1}`);
+      (this.x - 2 >= 0 && this.y + 1 >= 0 && this.x - 2 <= 7 && this.y + 1 <= 7) && possibleMoves.push(`${this.x - 2},${this.y + 1}`);
+      (this.x - 2 >= 0 && this.y - 1 >= 0 && this.x - 2 <= 7 && this.y - 1 <= 7) && possibleMoves.push(`${this.x - 2},${this.y - 1}`);
+      (this.x + 1 >= 0 && this.y + 2 >= 0 && this.x + 1 <= 7 && this.y + 2 <= 7) && possibleMoves.push(`${this.x + 1},${this.y + 2}`);
+      (this.x + 1 >= 0 && this.y - 2 >= 0 && this.x + 1 <= 7 && this.y - 2 <= 7) && possibleMoves.push(`${this.x + 1},${this.y - 2}`);
+      (this.x - 1 >= 0 && this.y + 2 >= 0 && this.x - 1 <= 7 && this.y + 2 <= 7) && possibleMoves.push(`${this.x - 1},${this.y + 2}`);
+      (this.x - 1 >= 0 && this.y - 2 >= 0 && this.x - 1 <= 7 && this.y - 2 <= 7) && possibleMoves.push(`${this.x - 1},${this.y - 2}`);
+    
     return possibleMoves;
   }
 }
