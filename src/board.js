dd2e416745a616d5586bf2988c9189411654412b
LukaszKrzyskow <56232227+LukaszKrzyskow@users.noreply.github.com>
2019-11-13 21:33:58 +0100

    Bishop
    * setup bishop
    
    * movements bishop
    
    * movment bishop
    
    earlier position can not be selected
    
    * delete bishop/ folder change
    
    * bishop movement, easier code

--- src/board.js
+++ src/board.js
@@ -1,3 +1,4 @@
+import Bishop from './pieces/bishop';
 import Pawn from './pieces/pawn';
 import Rook from './pieces/rook';
 import Knight from './pieces/knight';
@@ -33,10 +34,22 @@ board[knight.x][knight.y] = knight;
 knight = new Knight(7, 6, 'white');
 board[knight.x][knight.y] = knight;
 
+//Queen
 let queen = new Queen(0, 3, 'black');
 board[queen.x][queen.y] = queen;
 queen = new Queen(7, 3, 'white');
 board[queen.x][queen.y] = queen;
 
+//////////bishop
+let bishop = new Bishop(7, 2, 'white');
+board[bishop.x][bishop.y] = bishop;
+bishop = new Bishop(7, 5, 'white');
+board[bishop.x][bishop.y] = bishop;
+
+bishop = new Bishop(0, 2, 'black');
+board[bishop.x][bishop.y] = bishop;
+bishop = new Bishop(0, 5, 'black');
+board[bishop.x][bishop.y] = bishop;
+//////////bishop koniec
 
 export default board;
