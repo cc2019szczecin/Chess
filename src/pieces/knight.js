0a68e52d0102717b1f301b2e477efb61928225c1
Marta Jaszewska <jaszewska.marta@gmail.com>
2019-11-14 00:10:03 +0100

    collisions - first try

--- src/pieces/knight.js
+++ src/pieces/knight.js
@@ -9,10 +9,10 @@ class Knight extends Piece {
   }
   
   findLegalMoves() {
-  
-    const possibleMoves = [];
+      
+    let possibleMoves = [];
     const moves = [[2, -1], [2, 1], [1, -2], [1, 2], [-2, -1], [-2, 1], [-1, -2], [-1, 2]];
-         
+    
     moves.filter(move => (this.x + move[0] >= 0 && this.y + move[1] >= 0 && this.x + move[0] <= 7 && this.y + move[1] <= 7))
     .forEach(move => possibleMoves.push([move[0] + this.x, move[1] + this.y]));
 
@@ -24,9 +24,24 @@ class Knight extends Piece {
       // (this.x + 1 >= 0 && this.y - 2 >= 0 && this.x + 1 <= 7 && this.y - 2 <= 7) && possibleMoves.push(`${this.x + 1},${this.y - 2}`);
       // (this.x - 1 >= 0 && this.y + 2 >= 0 && this.x - 1 <= 7 && this.y + 2 <= 7) && possibleMoves.push(`${this.x - 1},${this.y + 2}`);
       // (this.x - 1 >= 0 && this.y - 2 >= 0 && this.x - 1 <= 7 && this.y - 2 <= 7) && possibleMoves.push(`${this.x - 1},${this.y - 2}`);
-    
+      
+     //na tych polach stoją figury w tym samym kolorze
+      let sameColor = possibleMoves.map(move => document.getElementById((`${move[0]},${move[1]}`)))
+      .filter(i => i.querySelector(`.${this.side}`))
+      .map(i => i.id)
+         
+      let legalMoves = possibleMoves.filter(move=> sameColor.some(i => i!=move.join(',')))
+      
+      console.log(legalMoves);
+
+      console.log(sameColor);
+
+      console.log(possibleMoves);
+      
     return possibleMoves;
   }
 }
 
 export default Knight;
+
+
